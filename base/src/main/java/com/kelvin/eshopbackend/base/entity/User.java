c417f43d1b677b14f233337739ebfca2ee7bd4ff
Kelvin Yeung <kelvin_yeung@qq.com>
2020-09-11 20:57:59 +0800

    拆分为微服务

---
+++ base/src/main/java/com/kelvin/eshopbackend/base/entity/User.java
@@ -0,0 +1,43 @@
+package com.kelvin.eshopbackend.base.entity;
+
+import com.kelvin.eshopbackend.base.enums.Gender;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author: Kelvin Yeuung
+ * @createdAt: 2020/8/9 16:35
+ * @description:
+ */
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(generator="sys_uid")
+    @GenericGenerator(name="sys_uid", strategy="uuid")
+    @Column(length = 32)
+    private String  id;
+
+    @Column(length = 255)
+    private String avatar;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthday;
+
+    private Gender gender;
+
+    @Column(length = 64)
+    private String nickName;
+    @Column(length = 128)
+    private String password;
+    @Column(length = 64)
+    private String username;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+
+}
